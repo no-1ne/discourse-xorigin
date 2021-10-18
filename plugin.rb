41bb72e9544e25058d39d6a63affc1f7f9c6cc53
no-1ne <dheerajkota@gmail.com>
2021-10-18 16:12:16 +0530

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,5 +6,6 @@
 Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => '*'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS, DELETE'})
-Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Headers' => 'Accept, Content-Type, Authorization, X-Requested-With'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Headers' => 'Content-Type, Cache-Control, X-Requested-With, X-CSRF-Token, Discourse-Present, User-Api-Key, User-Api-Client-Id, Authorization'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Credentials' => 'true'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Max-Age' => '7200'})
